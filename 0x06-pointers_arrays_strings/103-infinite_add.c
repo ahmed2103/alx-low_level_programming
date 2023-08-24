9424786cf3d1a8a3ccfb9c7a4711a295da3bd5ad
ahmed2103 <123449036+ahmed2103@users.noreply.github.com>
2023-08-24 12:21:31 +0300

    Update 103-infinite_add.c

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -46,23 +46,28 @@ char *rev_string(char *s)
  */
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-	int l1 = _strlen(n1), l2 = _strlen(n2), i = 0, a, b, c = 0;
+    int len1 = _strlen(n1), len2 = _strlen(n2), i = 0, a1, a2, carry = 0;
 
-	for (l1--, l2--, size_r--; l1 >= 0 || l2 >= 0 || c; l1--, l2--, i++)
-	{
-		if (i >= size_r)
-			return (0);
-		a = 0;
-		b = 0;
-		if (l1 >= 0)
-			a = n1[l1] - '0';
-		if (l2 >= 0)
-			b = n2[l2] - '0';
-		a = a + b + c;
-		c = a / 10;
-		a %= 10;
-		r[i] = a + '0';
-	}
-	r[i] = '\0';
-	return (rev_string(r));
+    for (len1--, len2--, size_r--; len1 >= 0 || len2 >= 0 || carry; len1--, len2--, i++)
+    {
+        if (i >= size_r)
+            return (0);
+        
+        a1 = 0;
+        a2 = 0;
+        
+        if (len1 >= 0)
+            a1 = n1[len1] - '0';
+        
+        if (len2 >= 0)
+            a2 = n2[len2] - '0';
+        
+        a1 = a1 + a2 + carry;
+        carry = a1 / 10;
+        a1 %= 10;
+        r[i] = a1 + '0';
+    }
+    
+    r[i] = '\0';
+    return (rev_string(r));
 }
