76aa8ff7835edd92a007f7d0bde533aba42304ef
ahmed2103 <123449036+ahmed2103@users.noreply.github.com>
2023-08-24 16:42:58 +0300

    Update 2-strncpy.c

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -11,6 +11,7 @@
 char *_strncpy(char *dest, char *src, int n)
 {
 	int i;
+
 	for (i = 0; i < n && src[i] != '\0'; i++)
 	{
 	dest[i] = src[i];
@@ -19,6 +20,6 @@ char *_strncpy(char *dest, char *src, int n)
 	{
 	dest[i] = '\0';
 	}
-    return dest;
+	return (dest);
 }
 
