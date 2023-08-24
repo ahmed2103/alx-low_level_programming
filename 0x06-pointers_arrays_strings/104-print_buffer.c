c3d9c6911f11b7c74741f290d6aa17ef42e416a3
ahmed2103 <123449036+ahmed2103@users.noreply.github.com>
2023-08-24 17:53:27 +0300

    Update 104-print_buffer.c

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -1,44 +1,25 @@
-#include "main.h"
-#include <stdio.h>
-
-/**
- * print_buffer - prints string buffer
- * @b: the buffer
- * @size: size of buffer to print
- *
- * Return: void
- */
-void print_buffer(char *b, int size)
+#include <stddef.h>
+include "main.h"
+char *rot13(char *s)
 {
-	int o = 0, i, j;
+    char *ptr = s;
+    char c;
 
-	if (size <= 0)
-	{
-		printf("\n");
-		return;
-	}
-	while (o < size)
-	{
-		j = size - o < 10 ? size - o : 10;
-		printf("%08x: ", o);
-		for (i = 0; i < 10; i++)
-		{
-			if (i < j)
-				printf("%02x", *(b + o + i));
-			else
-				printf("  ");
-			if (i % 2)
-				printf(" ");
-		}
-		for (i = 0; i < j; i++)
-		{
-			int c = *(b + o + i);
+    while ((c = *ptr)) {
+        if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z')) {
+            for (int i = 0; i < 13; i++) {
+                if (c == 'z') {
+                    c = 'a';
+                } else if (c == 'Z') {
+                    c = 'A';
+                } else {
+                    c++;
+                }
+            }
+        }
+        *ptr = c;
+        ptr++;
+    }
 
-			if (c < 32 || c > 132)
-				c = '.';
-			printf("%c", c);
-		}
-		printf("\n");
-		o += 10;
-	}
+    return s;
 }
