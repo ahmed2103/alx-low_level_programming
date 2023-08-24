6582b23f3d1be98b6fe898bf02f2d375a74b5d91
ahmed2103 <123449036+ahmed2103@users.noreply.github.com>
2023-08-24 12:06:18 +0300

    Update 103-infinite_add.c

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -46,20 +46,25 @@ char *reverse_str(char *str) {
  * Return: Pointer to the result buffer
  */
 char *infinite_add(char *n1, char *n2, char *r, int size_r) {
-    int length1 = str_length(n1) - 1;
-    int length2 = str_length(n2) - 1;
+    int length1; 
+    int length1;
     int i = 0;
     int carry = 0;
+    length1= str_length(n1) - 1;
+    length2= str_length(n2) - 1;
 
     while (length1 >= 0 || length2 >= 0 || carry) {
         if (i >= size_r - 1) {
             return NULL;
         }
+        int digit1;
+        int digit2;
+        
+            digit1 = (length1 >= 0) ? (n1[length1] - '0') : 0;
+            digit2 = (length2 >= 0) ? (n2[length2] - '0') : 0;
 
-        int digit1 = (length1 >= 0) ? (n1[length1] - '0') : 0;
-        int digit2 = (length2 >= 0) ? (n2[length2] - '0') : 0;
-
-        int sum = digit1 + digit2 + carry;
+        int sum;
+            sum= digit1 + digit2 + carry;
         carry = sum / 10;
         r[i] = (sum % 10) + '0';
         i++;
