9658e2895dc6e1fe5528490b832a8433738d3166
ahmed2103 <123449036+ahmed2103@users.noreply.github.com>
2023-08-24 23:34:52 +0300

    Update 7-leet.c

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -9,22 +9,23 @@
 
 char *leet(char *s)
 {
-    int i, c = 0;
-    const int sl[] = {97, 101, 111, 116, 108};
-    const int ul[] = {65, 69, 79, 84, 76};
-    const int n[] = {52, 51, 48, 55, 49};
+	int i, c = 0;
+	const int sl[] = {97, 101, 111, 116, 108};
+	const int ul[] = {65, 69, 79, 84, 76};
+	const int n[] = {52, 51, 48, 55, 49};
 
-    while (s[c] != '\0')
-    {
-        for (i = 0; i < 5; i++)
-        {
-            if (s[c] == sl[i] || s[c] == ul[i])
-            {
-                s[c] = n[i];
-                break;
-            }
-        }
-        ++c;
-    }
-    return s;
+	while (s[c] != '\0')
+	{
+		for (i = 0; i < 5; i++)
+
+			{
+			if (s[c] == sl[i] || s[c] == ul[i])
+				{
+				s[c] = n[i];
+				break;
+				}
+			}
+	++c;
+	}
+	return (s);
 }
