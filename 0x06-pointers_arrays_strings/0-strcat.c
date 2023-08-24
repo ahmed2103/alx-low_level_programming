ee10c510767bc7aad3671372a965f07047c45c48
ahmed2103 <123449036+ahmed2103@users.noreply.github.com>
2023-08-24 16:28:14 +0300

    Update 0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -9,26 +9,25 @@
  */
 char *_strcat(char *dest, char *src)
 {
-        int dest_len = 0;
-        int src_len = 0;
-        int i;
+	int dest_len = 0;
+	int src_len = 0;
+	int i;
 
-        while (dest[dest_len] != '\0')
-        {
-                dest_len++;
-        }
+	while (dest[dest_len] != '\0')
+	{
+		dest_len++;
+	}
+	while (src[src_len] != '\0')
+	{
+		src_len++;
+	}
 
-        while (src[src_len] != '\0')
-        {
-                src_len++;
-        }
+	for (i = 0; i < src_len; i++)
+	{
+		dest[dest_len + i] = src[i];
+	}
 
-         for (i = 0; i < src_len; i++)
-        {
-                dest[dest_len + i] = src[i];
-        }
+	dest[dest_len + src_len] = '\0';
 
-         dest[dest_len + src_len] = '\0';
-        
-         return (dest);
+	return (dest);
 }
