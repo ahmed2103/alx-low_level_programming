54aa5564088c2b7533165203582591f9371e6642
ahmed2103 <123449036+ahmed2103@users.noreply.github.com>
2023-08-24 17:28:36 +0300

    Update 4-rev_array.c

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -1,20 +1,56 @@
 /**
- * reverse_array - Reverses an array of integers
- *
- * @a: Array to reverse
- * @n: Size of the array
+ * reverse_array - Reverses the content of an array of integers.
+ * @a: Pointer to the array of integers.
+ * @n: Number of elements in the array.
  */
 void reverse_array(int *a, int n)
 {
-    int i;
-    int j;
+    int start = 0;
+    int end = n - 1;
+    int temp;
 
-    for (i = 0; i < n / 2; i++)
+    while (start < end)
     {
-        j = a[i];
-        a[i] = a[n - i - 1];
-        a[n - i - 1] = j;
+        // Swap the elements at start and end indices
+        temp = a[start];
+        a[start] = a[end];
+        a[end] = temp;
+
+        // Move the indices towards each other
+        start++;
+        end--;
     }
+}
+
+int main(void)
+{
+    int a[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 98, 1024, 1337};
+    int size = sizeof(a) / sizeof(int);
+
+    printf("Original array: ");
+    for (int i = 0; i < size; i++)
+    {
+        printf("%d", a[i]);
+        if (i < size - 1)
+            printf(", ");
+    }
+    printf("\n");
+
+    reverse_array(a, size);
+
+    printf("Reversed array: ");
+    for (int i = 0; i < size; i++)
+    {
+        printf("%d", a[i]);
+        if (i < size - 1)
+            printf(", ");
+    }
+    printf("\n");
+
+    return 0;
+}
+
+
 
 
 
