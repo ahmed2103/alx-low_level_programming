c23b3a454b7353fce688690171362891aa9a3e10
ahmed2103 <123449036+ahmed2103@users.noreply.github.com>
2023-08-24 12:37:40 +0300

    Update 103-infinite_add.c

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -1,27 +1,68 @@
-char *infinite_add(char *n1, char *n2, char *r, int size_r)
+#include "main.h"
+
+/**
+ * custom_strlen - Computes the length of a string.
+ * @s: The input string.
+ *
+ * Return: The length of the string.
+ */
+int custom_strlen(char *s)
 {
-    int len1 = _strlen(n1), len2 = _strlen(n2), i = 0, digit1, digit2, carry = 0;
+    int count = 0;
+
+    while (*s++)
+        count++;
+    return (count);
+}
+
+/**
+ * reverse_string - Reverses a string in place.
+ * @s: The string to be reversed.
+ *
+ * Return: A pointer to the reversed string.
+ */
+char *reverse_string(char *s)
+{
+    int length = custom_strlen(s), i = 0;
+    char temp;
+
+    for (i = 0; i < length / 2; i++)
+    {
+        temp = s[length - i - 1];
+        s[length - i - 1] = s[i];
+        s[i] = temp;
+    }
+    return (s);
+}
+
+/**
+ * add_infinite_strings - Adds two arbitrarily long strings of digits.
+ * @num1: The first digit string.
+ * @num2: The second digit string.
+ * @r: The buffer to store the result.
+ * @r: The size of the result buffer.
+ *
+ * Return: A pointer to the result buffer.
+ */
+char *add_infinite_strings(char *n1, char *n2, char *r, int size_r)
+{
+    int len1 = custom_strlen(n1), len2 = custom_strlen(n2), i = 0, digit1, digit2, carry = 0;
 
     for (len1--, len2--, size_r--; len1 >= 0 || len2 >= 0 || carry; len1--, len2--, i++)
     {
         if (i >= size_r)
             return (0);
-        
         digit1 = 0;
         digit2 = 0;
-        
         if (len1 >= 0)
             digit1 = n1[len1] - '0';
-        
         if (len2 >= 0)
             digit2 = n2[len2] - '0';
-        
         digit1 = digit1 + digit2 + carry;
         carry = digit1 / 10;
         digit1 %= 10;
         r[i] = digit1 + '0';
     }
-    
     r[i] = '\0';
-    return (rev_string(r));
+    return (reverse_string(r));
 }
