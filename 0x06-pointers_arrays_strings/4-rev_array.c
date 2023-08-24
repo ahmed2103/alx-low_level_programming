92e360de8634f37178262b03743ed1d1b93dec9a
ahmed2103 <123449036+ahmed2103@users.noreply.github.com>
2023-08-24 17:29:23 +0300

    Update 4-rev_array.c

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -11,47 +11,11 @@ void reverse_array(int *a, int n)
 
     while (start < end)
     {
-        // Swap the elements at start and end indices
         temp = a[start];
         a[start] = a[end];
         a[end] = temp;
 
-        // Move the indices towards each other
         start++;
         end--;
     }
 }
-
-int main(void)
-{
-    int a[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 98, 1024, 1337};
-    int size = sizeof(a) / sizeof(int);
-
-    printf("Original array: ");
-    for (int i = 0; i < size; i++)
-    {
-        printf("%d", a[i]);
-        if (i < size - 1)
-            printf(", ");
-    }
-    printf("\n");
-
-    reverse_array(a, size);
-
-    printf("Reversed array: ");
-    for (int i = 0; i < size; i++)
-    {
-        printf("%d", a[i]);
-        if (i < size - 1)
-            printf(", ");
-    }
-    printf("\n");
-
-    return 0;
-}
-
-
-
-
-
-
