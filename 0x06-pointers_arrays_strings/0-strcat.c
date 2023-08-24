5cbf504487af36a0921663de720b67f4cd6dce1f
ahmed2103 <123449036+ahmed2103@users.noreply.github.com>
2023-08-24 14:46:26 +0300

    Update 0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -13,22 +13,22 @@ int dest_len = 0;
 int src_len = 0;
 int i;
 
-while (dest[dest_len] != '\0')
+    while (dest[dest_len] != '\0')
 {
     dest_len++;
 }
 
-while (src[src_len] != '\0')
+    while (src[src_len] != '\0')
 {
     src_len++;
 }
 
-for (i = 0; i < src_len; i++)
+    for (i = 0; i < src_len; i++)
 {
     dest[dest_len + i] = src[i];
 }
 
 dest[dest_len + src_len] = '\0';
 
-return dest;
+return (dest);
 }
