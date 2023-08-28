2aa7f59d9c340bade9249eb2af8a5bafa4c92ab6
ahmed2103 <123449036+ahmed2103@users.noreply.github.com>
2023-08-28 16:22:05 +0300

    Update 1-memcpy.c

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -1,17 +1,20 @@
-#include "main.h"
+nclude "main.h"
 
 /**
  * _memcpy - Copies a specified number of bytes from source to destination
  * @dest: Destination memory address
- * @src: Source memory address
- * @n: Number of bytes to copy
- * Return: Pointer to the destination memory address
+ *    * @src: Source memory address
+ *     * @n: Number of bytes to copy
+ *      * Return: Pointer to the destination memory address
  */
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
-	unsigned int i;
+		unsigned int i;
 
-	for (i = 0; i < n; i++)
-		dest[i] = src[i];
+		for (i = 0; i < n; i++)
+		{
+			dest[i] = src[i];
+		}
 	return (dest);
 }
+
