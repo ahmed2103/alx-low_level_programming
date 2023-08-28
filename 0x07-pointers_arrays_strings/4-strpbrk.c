ef81e6ce9c08485f4c2814b5591cc6df1fa6fa97
ahmed2103 <123449036+ahmed2103@users.noreply.github.com>
2023-08-28 20:16:34 +0300

    Update 4-strpbrk.c

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -12,16 +12,19 @@ char *_strpbrk(char *s, char *accept)
 {
 	while (*s != '\0')
 	{
-	char *a = accept;
 
-	while (*a != '\0')
+		char *a = accept;
+
+		while (*a != '\0')
 		{
 		if (*a == *s)
 			return (s);
+
 			a++;
 		}
+
 	s++;
 	}
-
 	return (NULL);
 }
+
