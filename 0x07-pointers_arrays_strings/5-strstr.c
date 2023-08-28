106917bfb7ee83152f17abdc88a74221bfc05839
ahmed2103 <123449036+ahmed2103@users.noreply.github.com>
2023-08-28 20:34:09 +0300

    Update 5-strstr.c

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -10,22 +10,23 @@
  **/
 char *_strstr(char *haystack, char *needle)
 {
-    while (*haystack != '\0')
-    {
-        char *h = haystack;
-        char *n = needle;
+	while (*haystack != '\0')
+	{
+	char *h = haystack;
+	char *n = needle;
 
-        while (*n != '\0' && *h == *n)
-        {
-            h++;
-            n++;
-        }
+	while (*n != '\0' && *h == *n)
+	{
+		h++;
+		n++;
+	}
 
-        if (*n == '\0')
-            return haystack;
+	if (*n == '\0')
+		return (haystack);
 
-        haystack++;
-    }
+		haystack++;
+	}
 
-    return NULL;
+	return (NULL);
 }
+
