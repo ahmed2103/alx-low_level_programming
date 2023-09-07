38a337c856b205f1185d962c802ffef46dee35f9
ahmed2103 <123449036+ahmed2103@users.noreply.github.com>
2023-09-07 13:42:49 +0300

    Update 2-calloc.c

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -3,54 +3,24 @@
 #include "main.h"
 
 /**
- * string_nconcat - Concatenate two strings using at most n characters from s2
- * @s1: First string
- * @s2: String to add to the end of s1
- * @n: Maximum number of characters to concatenate from s2
+ * Allocate and initialize memory for an array.
  *
- * Return: Pointer to a new dynamically allocated string, or NULL if it fails
+ * @param nmemb Number of elements.
+ * @param size  Size of each element.
+ * @return      A pointer to the allocated memory, or NULL on failure.
  */
-char *string_nconcat(char *s1, char *s2, unsigned int n)
-{
-    char *nstr;
-    char *empt = "";
-    unsigned int i, len, j;
-    unsigned int size;
+void *_calloc(unsigned int nmemb, unsigned int size) {
+    void *ptr;
 
-    len = 0;
+    if (nmemb == 0 || size == 0)
+        return NULL;
 
-    if (s1 == NULL)
-        s1 = empt;
-    if (s2 == NULL)
-        s2 = empt;
+    ptr = malloc(nmemb * size);
 
-    while (s1[len] != '\0')
-        len++;
+    if (ptr == NULL)
+        return NULL;
 
-    size = (len + n) * sizeof(char) + 1;
-    nstr = malloc(size);
+    memset(ptr, 0, nmemb * size);
 
-    if (nstr == NULL)
-        return (NULL);
-
-    i = 0;
-
-    while (i < len && s1[i] != '\0')
-    {
-        nstr[i] = s1[i];
-        i++;
-    }
-
-    j = 0;
-
-    while (i < size - 1 && s2[j] != '\0' && j < n)
-    {
-        nstr[i] = s2[j];
-        i++;
-        j++;
-    }
-
-    nstr[i] = '\0';
-
-    return (nstr);
+    return ptr;
 }
