85eeea93fc033888cc0c10bc5eb2b38788952bbb
root <bekoahmed20@gmail.com>
2023-09-11 04:27:22 -0700

    dont forget your dreams

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - prints the name of the file  compiled from,followed by a new line
+ *
+ * Return: 0 always when success
+ */
+int main(void)
+{
+	puts(__FILE__);
+	return (0);
+}
