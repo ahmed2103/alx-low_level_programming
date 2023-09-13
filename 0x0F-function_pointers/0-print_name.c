35964b0fa4fe5e3dcf6ee6e690554422dd92087d
ahmed2103 <123449036+ahmed2103@users.noreply.github.com>
2023-09-13 22:30:40 +0300

    Update 0-print_name.c

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -2,11 +2,13 @@
 /**
  * print_name-function that prints a name.
  * @name:pointer to the character array
- * @(*f)(char *):function pointer to a function that is the second parameter.
+ * @f:function pointer to a function that is the second parameter.
  *
  * Return NOthing
  */
 void print_name(char *name, void (*f)(char *))
 {
+	if (f == NULL)
+		exit(EXIT_FAILURE);
 	f(name);
 }		
