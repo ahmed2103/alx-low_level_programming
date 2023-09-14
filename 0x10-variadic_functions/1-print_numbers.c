f1baf322bc4444a3c10cf9834b705585e4a13f87
ahmed2103 <123449036+ahmed2103@users.noreply.github.com>
2023-09-14 20:49:21 +0300

    Update 1-print_numbers.c

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -18,6 +18,7 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	{
 		printf("%d%s", va_arg(args, int), separator);
 	}
-	printf("%d\n", va_arg(args, int));
+	printf("%d", va_arg(args, int));
+	putchar('\n');
 	va_end(args);
 }
