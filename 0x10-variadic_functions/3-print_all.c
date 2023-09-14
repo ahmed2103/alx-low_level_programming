68711924670c5c41470f947f862fc8515c10047c
ahmed2103 <123449036+ahmed2103@users.noreply.github.com>
2023-09-14 23:08:18 +0300

    Update 3-print_all.c

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -3,13 +3,13 @@
 
 /**
  * print_all - function that prints anything.
- * @format is a list of types of arguments passed to the function.
+ * @format: is a list of types of arguments passed to the function.
  */
 void print_all(const char * const format, ...)
 {
-    va_list args;
-    int i;
-    char *s;
+	va_list args;
+	int i;
+	char *s;
 
 va_start(args, format);
 	while (format && format[i])
