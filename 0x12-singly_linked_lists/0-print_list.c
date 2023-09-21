2414879ff2e392cb5172743dd43127fb91fda2fe
ahmed2103 <123449036+ahmed2103@users.noreply.github.com>
2023-09-21 22:34:53 +0300

    Update 0-print_list.c

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -4,9 +4,9 @@
 
 /**
  *print_list -  prints all the elements of a list_t list.
- *@h: pointer to the linked list head.
+ *@h: pointer to the list_t list to print.
  *
- * Return: the number of nodes in the giben linked list
+ * Return: the number of nodes in the linked list printed.
  */
 size_t print_list(const list_t *h)
 {
