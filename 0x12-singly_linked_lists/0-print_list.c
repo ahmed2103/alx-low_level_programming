c456d7a67401a3b5dd3963eddf0499cd08ade5e5
root <bekoahmed20@gmail.com>
2023-09-21 12:22:55 -0700

    linked losts task

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stddef.h>
+#include "lists.h"
+
+/**
+ *print_list -  prints all the elements of a list_t list.
+ *@h: pointer to the linked list head.
+ *
+ * Return: the number of nodes in the giben linked list
+ */
+size_t print_list(const list_t *h)
+{
+	size_t num = 0;
+	
+	if (!h)
+		return (-1);
+	while (h)
+	{
+		if (!(h->str))
+		{
+			printf("[%d] %s\n", 0, "(nil)");
+		}
+		else
+		{
+			printf("[%d] %s\n", h->len, h->str);
+
+		}
+		h = h->next;
+		++num;
+	}
+	return (num);
+}
