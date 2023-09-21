cba7671cc72d240340a5e87813926e1bb0efd2a2
ahmed2103 <123449036+ahmed2103@users.noreply.github.com>
2023-09-21 22:44:09 +0300

    Update 0-print_list.c

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -14,16 +14,11 @@ size_t print_list(const list_t *h)
 	while (h)
 	{
 		if (!h->str)
-		{
 			printf("[0] (nil)\n");
-		}
 		else
-		{
 			printf("[%d] %s\n", h->len, h->str);
-
-		}
 		h = h->next;
-		++num;
+		num++;
 	}
 	return (num);
 }
