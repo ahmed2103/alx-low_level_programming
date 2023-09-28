a5e96d80edd5279f6e1c4eb689e9afcdd941b071
ahmed2103 <123449036+ahmed2103@users.noreply.github.com>
2023-09-28 16:48:25 +0300

    Update 0-binary_to_uint.c

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -14,9 +14,9 @@ unsigned int binary_to_uint(const char *b)
 
 	if (!b)
 		return (0);
-	for(len = 0; b[len];++len)
+	for(len = 0; b[len]; ++len)
 	{
-		if(b[len] != 0 && b[len] != 1)
+		if(b[len] != '0' && b[len] != '1')
 			return (0);
 	}
 	for (--len, result = 0;len >= 0; pow *=2, --len)
