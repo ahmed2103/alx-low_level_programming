634c11edb409b616b629a0c80cf44a15a54c81b7
ahmed2103 <123449036+ahmed2103@users.noreply.github.com>
2023-09-28 16:13:18 +0300

    Update 4-clear_bit.c

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -7,7 +7,7 @@
  *
  * Returns: 1 if it worked, or -1 if an error occurred.
  */
-int set_bit(unsigned long int *n, unsigned int index)
+int clear_bit(unsigned long int *n, unsigned int index)
 {	
 	if (index > sizeof(*n))
 		return (-1);
