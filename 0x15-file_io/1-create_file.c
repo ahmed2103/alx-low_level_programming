08709e52f99a4be847584c0e311c50769dece710
ahmed2103 <123449036+ahmed2103@users.noreply.github.com>
2023-10-02 20:36:01 +0300

    Update 1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -12,29 +12,29 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-  int fd;
-  ssize_t w;
-  size_t len;
+	int fd;
+	ssize_t w;
+	size_t len;
 
-  if (!filename)
-    return (-1);
+	if (!filename)
+		return (-1);
 
-  if (!text_content)
-    text_content = "";
+	if (!text_content)
+		text_content = "";
 
-  len = strlen(text_content);
+	len = strlen(text_content);
 
-  fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
-  if (fd == -1)
-    return (-1);
+	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
+	if (fd == -1)
+		return (-1);
 
-  w = write(fd, text_content, len);
-  if (w == -1)
-  {
-    close(fd);
-    return (-1);
-  }
+	w = write(fd, text_content, len);
+	if (w == -1)
+	{
+	close(fd);
+	return (-1);
+	}
 
-  close(fd);
-  return (1);
+	close(fd);
+	return (1);
 }
