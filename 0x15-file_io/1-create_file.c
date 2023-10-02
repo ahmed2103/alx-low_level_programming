5be558c8b6af5a07f608b6957999f96284fb38c8
ahmed2103 <123449036+ahmed2103@users.noreply.github.com>
2023-10-02 20:02:37 +0300

    Update 1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,31 +1,40 @@
 #include <fcntl.h>
 #include <unistd.h>
+#include <string.h>
+#include "main.h"
 
 /**
  * create_file - creates file.
  * @filename: the name of the file.
- * @text_content:  is a NULL terminated string to write to the file.
- * Returns: 1 on success.
- * OR: -1 on failure.
+ * @text_content: is a NULL terminated string to write to the file.
+ *
+ * Return: 1 on success, -1 on failure.
  */
 int create_file(const char *filename, char *text_content)
 {
-	int fd;
-	int w, len = 0;
-	if (filename == NULL)
-	       return (-1);
-	if (!text_content)
-	{
-		for (len =0; text_content[len]; ++len)
-		;
-	}	
-	int fd = open("foo.txt", O_WRONLY | O_CREAT | O_TRUNC, 0600);
-	if (fd == -1)
-		return (-1);
-	w = write(fd, text_content, len);
-	if ( w == -1)
-		return (-1);
-	close(fd);
-	return (1);
+  int fd;
+  ssize_t w;
+  size_t len;
+
+  if (!filename)
+    return (-1);
+
+  if (!text_content)
+    text_content = "";
+
+  len = strlen(text_content);
+
+  fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
+  if (fd == -1)
+    return (-1);
+
+  w = write(fd, text_content, len);
+  if (w == -1)
+  {
+    close(fd);
+    return (-1);
+  }
+
+  close(fd);
+  return (1);
 }
-	
