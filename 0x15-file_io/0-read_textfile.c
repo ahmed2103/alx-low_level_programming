a7f0f137e71f21bacd74c4032a0f5dbeadf47d7f
ahmed2103 <123449036+ahmed2103@users.noreply.github.com>
2023-10-02 19:41:00 +0300

    Update 0-read_textfile.c

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -11,23 +11,40 @@
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	int fd;
-	ssize_t sz;
-	char *buffer = malloc((sizeof(char) * letters) + 1);
+  int fd;
+  ssize_t sz;
+  char *buffer;
 
-	if (!filename)
-		return (0);
-	fd = open(filename, O_RDONLY);
-	if (fd < 0)
-		return (0);
-	read(fd, buffer, sizeof(buffer));
-	close(fd);
-	buffer[sz] = '\0';
-	sz = write(STDOUT_FILENO, buffer, sizeof(buffer) - 1)
-	free(buffer);
-	if (sz <= 0)
-		return (0);
-	
-	return (sz);
+  if (!filename)
+    return (0);
+
+  fd = open(filename, O_RDONLY);
+  if (fd < 0)
+    return (0);
+
+  buffer = malloc((sizeof(char) * letters) + 1);
+  if (!buffer)
+  {
+    close(fd);
+    return (0);
+  }
+
+  sz = read(fd, buffer, letters);
+  if (sz < 0)
+  {
+    free(buffer);
+    close(fd);
+    return (0);
+  }
+
+  buffer[sz] = '\0';
+
+  sz = write(STDOUT_FILENO, buffer, sz);
+  free(buffer);
+  close(fd);
+
+  if (sz <= 0)
+    return (0);
+
+  return (sz);
 }
-
