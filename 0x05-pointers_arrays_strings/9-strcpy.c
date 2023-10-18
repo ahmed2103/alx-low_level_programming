aef5738cdd688ffda82e75552e4c43390afa08be
ahmed2103 <123449036+ahmed2103@users.noreply.github.com>
2023-10-18 08:50:03 -0400

    Update 9-strcpy.c

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -10,17 +10,16 @@
  */
 char *_strcpy(char *dest, char *src)
 {
-    char *original_dest = dest; // Store the original destination pointer
+    char *original_dest = dest;
 
-    // Copy characters from src to dest, including the null terminator
     while (*src != '\0') {
         *dest = *src;
         dest++;
         src++;
     }
 
-    *dest = '\0'; // Add the null terminator at the end
+    *dest = '\0';
 
-    return original_dest; // Return the pointer to the beginning of the destination buffer
+    return original_dest;
 }
 
