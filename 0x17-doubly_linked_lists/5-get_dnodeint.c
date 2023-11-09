87170038f550bb3ff7a602565d77de096a852ec7
ahmed2103 <123449036+ahmed2103@users.noreply.github.com>
2023-11-09 21:00:49 +0200

    Update 5-get_dnodeint.c

--- 0x17-doubly_linked_lists/5-get_dnodeint.c
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -1,7 +1,7 @@
 #include "lists.h"
 
 /**
- * get_dnodeint_at_index - Gets the node at a given index in a doubly linked list.
+ * get_dnodeint_at_index - Gets the node at a given index in a linked list.
  * @head: Pointer to the head of the linked list.
  * @index: Index of the node to retrieve.
  *
