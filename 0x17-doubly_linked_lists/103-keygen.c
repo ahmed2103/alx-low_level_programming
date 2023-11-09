17760eaa119a4b0a40d89656d6fa13c356bccb2c
ahmed2103 <123449036+ahmed2103@users.noreply.github.com>
2023-11-09 22:00:29 +0200

    Update 103-keygen.c

--- 0x17-doubly_linked_lists/103-keygen.c
+++ 0x17-doubly_linked_lists/103-keygen.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+
 /**
  * main - Takes a command line argument.
  * @argc: Number of command line arguments.
@@ -8,48 +9,47 @@
  *
  * Return: 0 on success, 1 otherwise.
  */
-
 int main(int argc, char *argv[]) {
-    size_t i, j;
-    int rnd = 0;
-    char *h, *uname, pw[7], max_char;
+	size_t i, j;
+	int rnd = 0;
+	char *h, *uname, pw[7], max_char;
 
-    h = "A-CHRDw87lNS0E9B2TibgpnMVys5XzvtOGJcYLU+4mjW6fxqZeF3Qa1rPhdKIouk";
+	h = "A-CHRDw87lNS0E9B2TibgpnMVys5XzvtOGJcYLU+4mjW6fxqZeF3Qa1rPhdKIouk";
 
-    if (argc != 2)
-        exit(1);
+	if (argc != 2)
+		exit(1);
 
-    uname = argv[1];
+	uname = argv[1];
 
-    j = (strlen(uname) ^ 0x3b) & 0x3f;
-    pw[0] = h[j];
+	j = (strlen(uname) ^ 0x3b) & 0x3f;
+	pw[0] = h[j];
 
-    for (j = i = 0; uname[i]; i++)
-        j += uname[i];
-    pw[1] = h[(j ^ 0x4f) & 0x3f];
+	for (j = i = 0; uname[i]; i++)
+		j += uname[i];
+	pw[1] = h[(j ^ 0x4f) & 0x3f];
 
-    for (j = 1, i = 0; uname[i]; i++)
-        j *= uname[i];
-    pw[2] = h[(j ^ 0x55) & 0x3f];
+	for (j = 1, i = 0; uname[i]; i++)
+		j *= uname[i];
+	pw[2] = h[(j ^ 0x55) & 0x3f];
 
-    for (max_char = uname[0], i = 0; i < strlen(uname); i++)
-        if (uname[i] > max_char)
-            max_char = uname[i];
+	for (max_char = uname[0], i = 0; i < strlen(uname); i++)
+		if (uname[i] > max_char)
+			max_char = uname[i];
 
-    srand(max_char ^ 0xe);
-    j = rand();
-    pw[3] = h[j & 0x3f];
+	srand(max_char ^ 0xe);
+	j = rand();
+	pw[3] = h[j & 0x3f];
 
-    for (j = i = 0; uname[i]; i++)
-        j += (uname[i] * uname[i]);
-    pw[4] = h[(j ^ 0xef) & 0x3f];
+	for (j = i = 0; uname[i]; i++)
+		j += (uname[i] * uname[i]);
+	pw[4] = h[(j ^ 0xef) & 0x3f];
 
-    for (max_char = uname[0]; max_char > 0; max_char--)
-        rnd = rand();
-    pw[5] = h[(rnd ^ 0xe5) & 0x3f];
+	for (max_char = uname[0]; max_char > 0; max_char--)
+		rnd = rand();
+	pw[5] = h[(rnd ^ 0xe5) & 0x3f];
 
-    pw[6] = '\0';
-    printf("%s", pw);
+	pw[6] = '\0';
+	printf("%s", pw);
 
-    return 0;
+	return (0);
 }
