2e5cf76f11827f6e4c40ab09a1429b169c97380d
ahmed2103 <123449036+ahmed2103@users.noreply.github.com>
2023-11-09 20:37:12 +0200

    Create 1-dlistint_len.c

---
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * dlistint_len - Returns the length of a doubly linked list.
+ * @h: Head of the doubly linked list.
+ * Return: Size of the linked list.
+ */
+size_t dlistint_len(const dlistint_t *h)
+{
+    const dlistint_t *tmp;
+    size_t count = 0;
+
+    tmp = h;
+
+    while (tmp != NULL)
+    {
+        count++;
+        tmp = tmp->next;
+    }
+
+    return (count);
+}
