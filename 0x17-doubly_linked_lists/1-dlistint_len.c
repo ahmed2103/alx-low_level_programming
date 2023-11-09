6bbfb964407bbf4be6314770f763b550661b2322
ahmed2103 <123449036+ahmed2103@users.noreply.github.com>
2023-11-09 20:41:22 +0200

    Update 1-dlistint_len.c

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -7,16 +7,14 @@
  */
 size_t dlistint_len(const dlistint_t *h)
 {
-    const dlistint_t *tmp;
-    size_t count = 0;
+	const dlistint_t *tmp;
+	size_t count = 0;
 
-    tmp = h;
-
-    while (tmp != NULL)
-    {
-        count++;
-        tmp = tmp->next;
-    }
-
-    return (count);
+	tmp = h;
+	while (tmp != NULL)
+	{
+		count++;
+		tmp = tmp->next;
+	}
+	return (count);
 }
