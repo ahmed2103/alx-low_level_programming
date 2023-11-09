a46b9c6aec035e063a13bf68388cd5c9677a8c97
ahmed2103 <123449036+ahmed2103@users.noreply.github.com>
2023-11-09 21:24:21 +0200

    Update 2-add_dnodeint.c

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -8,27 +8,23 @@
  */
 dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
-	dlistint_t *new;  /* Pointer to the new node */
+	dlistint_t *new;
 
-	/* Allocate memory for the new node */
 	new = malloc(sizeof(dlistint_t));
 	if (new == NULL)
 		return (NULL);
 
-	/* Set the values of the new node */
 	new->n = n;
 	new->next = NULL;
 	new->prev = NULL;
 
-	/* Update pointers if the list is not empty */
 	if (*head != NULL)
 	{
 		(*head)->prev = new;
 		new->next = *head;
 	}
 
-	/* Update the head to point to the new node */
 	*head = new;
 
-	return (new);  /* Return the address of the new node */
+	return (new);
 }
