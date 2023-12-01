1711685a524b8f0c2d3c7e4bf9e13dc80891a06a
ahmed2103 <123449036+ahmed2103@users.noreply.github.com>
2023-12-01 17:10:04 +0200

    Update 3-hash_table_set.c

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -22,9 +22,9 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		{
 			free(checker->value);
 			checker->value = strdup(value); /*update*/
-				if (!checker->value)
-					return (0);
-				return (1);
+			if (!checker->value)
+				return (0);
+			return (1);
 		}
 		cheker = checker->next;
 	}
