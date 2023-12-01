e311ba6cbdb051ce762697c768f8e56253a0c774
ahmed2103 <123449036+ahmed2103@users.noreply.github.com>
2023-12-01 17:49:35 +0200

    Create 4-hash_table_get.c

---
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -0,0 +1,29 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_get - retrieves a value associated with a key.
+ * @ht: hash table you want to look into.
+ * @key: the key for the value.
+ *
+ * Returns the value associated with the element.
+ * OR NULL if key couldn’t be found.
+ */
+char *hash_table_get(const hash_table_t *ht, const char *key);
+{
+	unsigned long int idx;
+	hash_node_t *getter;
+	
+	if (!key || !ht->size || !ht)
+		return (NULL);
+
+	idx = key_index((const unsigned char *)key, ht->size);
+	getter = ht->array[idx];
+	
+	while (getter)
+	{
+		if (getter->key == key)
+			return(getter->value);
+		getter = getter->next;
+	}
+	return (NULL);
+}
