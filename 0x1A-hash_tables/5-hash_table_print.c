ea6c7ead5154f4957760e7168f0eca7c6db72a82
ahmed2103 <123449036+ahmed2103@users.noreply.github.com>
2023-12-01 19:44:32 +0200

    Create 5-hash_table_print.c

---
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -0,0 +1,32 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_print - prints hash table
+ * @ht: hash table to print
+ */
+void hash_table_print(const hash_table_t *ht)
+{
+	unsigned int i;
+	unsigned int lastKey = 0;
+	hash_node_t *printer;
+
+	if (ht == NULL)
+		return;
+
+	while (lastKey < ht->size && ht->array[lastKey] == NULL)
+		lastKey++;
+
+	printf("{");
+	for (i = 0; i < ht->size; i++)
+	{
+	printer = ht->array[i];
+	while (printer)
+		{
+		printf("'%s': '%s'", printer->key, printer->value);
+		if ((i != lastKey - 1) || (printer->next != NULL))
+			printf(", ");
+		printer = printer->next;
+		}
+	}
+	printf("}\n");
+}
