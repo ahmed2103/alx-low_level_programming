619a3c63c80c53ccc003960931acff10cfaa0d2e
ahmed2103 <123449036+ahmed2103@users.noreply.github.com>
2023-12-01 20:38:18 +0200

    Update 6-hash_table_delete.c

--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -20,7 +20,7 @@ void hash_table_delete(hash_table_t *ht)
 			free(deleter->key);
 			free(deleter->value);
 			free(deleter);
-			deleter = temp->next;
+			deleter = tmp;
 		}
 	free(ht->array);
 	free(ht);
