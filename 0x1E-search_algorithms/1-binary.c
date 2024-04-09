43d3977ecf92a31a3311263f53448ca175dc24e3
kali <kali@kali.kali>
2024-04-09 17:26:37 +0200

    no return

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -11,31 +11,31 @@
 
 int binary_search(int *array, size_t size, int value)
 {
-    size_t left, right, mid, i;
+	size_t left, right, mid, i;
 
-    if (!array)
-        return (-1);
+	if (!array)
+		return (-1);
 
-    left = 0;
-    right = size - 1;
+	left = 0;
+	right = size - 1;
 
-    while (left <= right)
-    {
-        printf("Searching in array:");
-        for(i = left; i <= right; i++)
-        {
-            printf(" %d", array[i]);
-            if (i < right)
-                printf(",");
-        }
-        printf("\n");
-        mid = (left + right) / 2;
-        if (array[mid] > value)
-            right = mid - 1;
-        else if (array[mid] < value)
-            left = mid + 1;
-        else
-            return (mid);
-    }
-    return (-1);
+	while (left <= right)
+	{
+		printf("Searching in array:");
+		for (i = left; i <= right; i++)
+		{
+			printf(" %d", array[i]);
+			if (i < right)
+				printf(",");
+		}
+	printf("\n");
+	mid = (left + right) / 2;
+	if (array[mid] > value)
+		right = mid - 1;
+	else if (array[mid] < value)
+		left = mid + 1;
+	else
+		return (mid);
+	}
+	return (-1);
 }
